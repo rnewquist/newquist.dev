a8a62fc91a7d5370ffd293ee749c52409638e2dc
Ryan Newquist <newqust2@illinois.edu>
2022-06-14 16:27:04 -0400

    added about me

--- lib/sections/about_section.dart
+++ lib/sections/about_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:newquist_dev/util/constants.dart';
+import 'package:newquist_dev/work_card.dart';
 
 class AboutSection extends StatelessWidget {
   const AboutSection({Key? key}) : super(key: key);
@@ -26,6 +28,50 @@ class AboutSection extends StatelessWidget {
             'ABOUT ME',
             style: title,
           ),
+        ),
+        const Text(
+          'Mobile Developer since 2015, primarily focused with development in Flutter\n'
+          'Built numerous mobile applications from the ground up as well as ported applications to new standards\n'
+          'Trained in Scrum Agile methodologies, having been a scrum master for numerous projects\n'
+          'Obtained a Masters in Computer Sciences from University of Louisville\n'
+          'Obtained Bachelors in Engineering Mechanics from University of Illinois Urbana-Champaign',
+          style: header2,
+          textAlign: TextAlign.center,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(
+            'WORK HISTORY',
+            style: title,
+          ),
+        ),
+        WorkCard(
+          child: Image.asset('assets/ge.jpeg'),
+          startDate: DateTime(2019, 4),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        WorkCard(
+          child: SvgPicture.asset('assets/cbs.svg'),
+          endDate: DateTime(2019, 4),
+          startDate: DateTime(2017, 1),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        WorkCard(
+          child: Image.asset('assets/vitronic.jpeg'),
+          endDate: DateTime(2016, 12),
+          startDate: DateTime(2016, 7),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        WorkCard(
+          child: Image.asset('assets/horizonhobby.jpeg'),
+          endDate: DateTime(2016, 4),
+          startDate: DateTime(2015, 6),
         ),
       ],
     );
